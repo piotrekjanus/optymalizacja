6b641b873f078fd0046eaaac83060c927bebbc29
Piotr <piotrek.janus96@gmail.com>
2019-12-29 18:58:48 +0100

    working lasso

--- lars.py
+++ lars.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 # Regularization for matrix inverting
 MAT_REG = 1e-12
+# Resolution for Lars
+LARS_RES = 1e-12
 
 def plot_path(beta_path):
     sum_abs_coeff = np.sum(np.abs(beta_path), 1)
@@ -15,25 +17,33 @@ def update_save(history):
     #TODO
     pass
 
+def check_stop_criterions(params):
+    force_stop = False
+    if np.abs(params["c_max"][-1] / params["Aa"][-1] - np.min([params["gamma"][-1], params["gamma_tilde"][-1]])) < LARS_RES:
+        print("Exact matching")
+        force_stop = True
+    if params["mse"][-1] > params["mse"][-2]:
+        print("MSE increased")
+        force_stop = True
+    return force_stop
 
-def lars(x, y):
+
+def lars(x, y, alg_type = "lars"):
+
     assert x is not None, "x should not be empty"
     assert y is not None, "y should not be empty"
     assert y.ndim == 2, "y is expected to be 2D array"
-
+    assert alg_type.lower() in ["lasso", "lars"], 'only ["lars", "lar"] types are available, please select right type'
     # reg_factor = []
     # stop_condition = []
 
-    # xtx =  np.dot(x.T, x) 
-    
     my = np.mean(y)
 
     # Calculate residuals
     res = y - my
     
     n = x.shape[0]
-    m = x.shape[1]
-    
+    m = x.shape[1]    
 
     var_col = np.var(x, axis = 0)
     
@@ -46,28 +56,16 @@ def lars(x, y):
 
     # Suppose that µA is the current LARS estimate 
     mu_a = np.zeros((n,1))
-    # Then the next step of the LARS algorithm updates µ
-    # mu_a_plus = 0
-    # mu_a_OLS = 0 
        
-    # to liczymy
     beta = np.zeros((x.shape[1], 1))
-    # beta_new = beta
-    # beta_OLS = beta
     
-    params = ["active_set", "add", "drop", "beta_norm", "beta", "b", "mu", "beta_ols_norm", "beta_ols",
-              "b_ols", "mu_ols", "mse", "r_square"]
+    params = ["active_set", "c_max", "Aa", "gamma", "gamma_tilde"]
     
     history = dict()
     for key in params:
         history[key] = []
     
     history["beta"] = [np.zeros((m, 1))]
-    history["b"] = my
-    history["mu"] = my
-    history["r_square"] = my
-    history["b_ols"] = my
-    history["mu_ols"] = my * np.ones(y.shape)
     history["mse"] = [np.power(res, 2).mean()]
     
     c = 0
@@ -75,9 +73,11 @@ def lars(x, y):
     # max(abs(c))
     c_max = 0
 
+    # used for lasso version od LARS
+    drop = []  
+
     i = 0
-    while i<100:
-#        check_criterions()
+    while i<12:
         
         # eq. 2.8
         # vector of current correlations
@@ -87,13 +87,21 @@ def lars(x, y):
         c_max_temp = np.max(np.abs(c[inactive_set]))
         c_max, c_max_ind = c_max_temp, np.where(np.abs(c) == c_max_temp)[0]
 
-        # print(c_max_ind + 1)
         # eq. 2.9 
         # active set A is the set of indices
         # corresponding to covariates with the greatest absolute current correlations
         active_set = np.append(active_set, c_max_ind).astype(dtype = np.int16)
         inactive_set = np.setdiff1d(possible_var, active_set)
-        
+
+        # LASSO
+        if alg_type == 'lasso':
+            if len(drop) > 0 and len(np.where(drop == c_max_ind)) == 0:
+                del active_set[active_set == c_max_ind]
+
+            if len(drop) > 0:
+                active_set = np.setdiff1d(active_set, drop)
+                inactive_set = np.unique(np.append(inactive_set, drop))
+
         # eq. 2.10
         s = np.sign(c[active_set])
 
@@ -139,14 +147,40 @@ def lars(x, y):
             gamma = np.min(gamma_[gamma_ > 0])
         else:
             gamma = c_max/Aa
-            i = 100
-            
+
+        
         d = np.zeros((m, 1))
         d[active_set, :] = s * wa
 
+        tmp = np.zeros((m, 1))
+        # eq 3.4
+        tmp[active_set, :] = -1 * beta[active_set] / d[active_set, :] 
+        tmp2 = tmp[tmp > 0]
+        
+        drop = []
+        # eq 3.5
+        gamma_tilde = np.inf
+        if len(tmp2) > 0 and gamma >= np.min(tmp2):
+            # eq 3.5
+            gamma_tilde = np.min(tmp2)
+            # eq 3.6 
+            drop = np.where(tmp == gamma_tilde)
+            
+        if alg_type == 'lars':
+            mu_a_ = mu_a + gamma * ua
+            beta_ = beta + gamma * d
+            drop = []
+
+        if alg_type == 'lasso':
+            mu_a_ = mu_a + np.min([gamma, gamma_tilde]) * ua
+            beta_ = beta + np.min([gamma, gamma_tilde]) * d
+            active_set = np.setdiff1d(active_set, drop)
+            inactive_set = np.setdiff1d(possible_var, active_set)
+            beta_[drop] = 0
+
         # eq 2.12 2.19 2.21
-        mu_a = mu_a + gamma * ua
-        beta = beta + gamma * d 
+        mu_a = mu_a_
+        beta = beta_ 
         
         # mu_a = mu_a + c_max/Aa * ua 
         # beta = beta + c_max/Aa * d 
@@ -156,9 +190,16 @@ def lars(x, y):
         # beta = beta_new
         history["mse"] = history["mse"] + [MSE]
         history["beta"] = history["beta"] + [beta] 
+        history["gamma"] = history["gamma"] + [gamma]
+        history["gamma_tilde"] = history["gamma_tilde"] + [gamma]
+        history["Aa"] = history["Aa"] + [Aa]
+        history["c_max"] = history["c_max"] + [c_max]
         i += 1
+        if check_stop_criterions(history):
+            break
+
     print(history["mse"])
-    d = pd.DataFrame(np.round(history["beta"], 2).reshape((11, 10)))
+    d = pd.DataFrame(np.round(history["beta"], 2).reshape((i+1, 10)))
     print(d)
 
     return np.squeeze(history["beta"])
@@ -169,5 +210,7 @@ if __name__ == "__main__":
     # x = scaler.transform(x)
     y = y.reshape((-1,1))
     # y = StandardScaler().fit_transform(y)
-    beta_path = lars(x, y) 
+    # beta_path = lars(x, y, 'lars') 
+    # plot_path(beta_path)
+    beta_path = lars(x, y, 'lasso')
     plot_path(beta_path)
